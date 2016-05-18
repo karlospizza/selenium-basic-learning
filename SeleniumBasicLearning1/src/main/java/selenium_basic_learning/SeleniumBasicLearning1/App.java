db9513c2b17901d1eebd33b6d21aeba056367bae
karlospizza <morganbons@mail.ru>
2016-05-18 15:34:10 +0300

    initialproject

---
+++ SeleniumBasicLearning1/src/main/java/selenium_basic_learning/SeleniumBasicLearning1/App.java
@@ -0,0 +1,13 @@
+package selenium_basic_learning.SeleniumBasicLearning1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
